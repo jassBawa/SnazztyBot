8cf950d2a59b9f1319f047f7bd06597fa8ee1dac
jassBawa <jasspreetsbawa@gmail.com>
2025-09-20 00:25:16 +0530

    basic functions added

---
+++ callback-ids.ts
@@ -0,0 +1,8 @@
+
+export const CallbackIds = {
+    GENERATE_WALLET: "generate_wallet",
+    SHOW_PUBLIC_KEY: 'show_public_key',
+    SEND_SOL: "send_sol",
+    AIRDROP_SOL: "airdrop_sol",
+    GET_USER_BALANCE: "get_user_balance"
+}
