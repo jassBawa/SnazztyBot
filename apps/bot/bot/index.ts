2f3e24ae8e4e148c44819acabb7476b07355eb9a
jassBawa <jasspreetsbawa@gmail.com>
2025-10-28 05:39:10 +0530

    chore: removed env file

--- apps/bot/bot/index.ts
+++ apps/bot/bot/index.ts
@@ -1,8 +1,8 @@
 import { Telegraf } from "telegraf";
-import "dotenv/config";
+import { config } from 'dotenv';
+config();
 
 const botToken = process.env.BOT_TOKEN;
-
 if (!botToken) {
   console.log("Bot Token not found");
   throw new Error("Bot token not found");
